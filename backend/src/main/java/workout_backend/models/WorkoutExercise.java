c7c1744f01a6e45fb10dbc4a7d25f23ed4a3a675
Janne Johansson <jjouhansson@hotmail.fi>
2021-03-22 00:11:31 +0200

    Restored an oopsie where I deleted a model.

---
+++ backend/src/main/java/workout_backend/models/WorkoutExercise.java
@@ -0,0 +1,47 @@
+package workout_backend.models;
+
+public class WorkoutExercise {
+    private String name;
+    private int sets;
+    private int reps;
+    private int weight;
+
+    public WorkoutExercise(String name, int sets, int reps, int weight) {
+        this.name = name;
+        this.sets = sets;
+        this.reps = reps;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSets() {
+        return sets;
+    }
+
+    public void setSets(int sets) {
+        this.sets = sets;
+    }
+
+    public int getReps() {
+        return reps;
+    }
+
+    public void setReps(int reps) {
+        this.reps = reps;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
